ff36134158a8eb366d776ffb6e4ecf0189869b42
Vladislav Volkov <wwwvladislav@gmail.com>
2024-04-06 20:46:53 +0300

    Unit test for condition node

--- tests/basic_tests.cpp
+++ tests/basic_tests.cpp
@@ -103,3 +103,27 @@ TEST_CASE("Parallel fallback node", "[control]") {
   REQUIRE(seq() == bhv::status::failure);
   REQUIRE(n == 4);
 }
+
+TEST_CASE("Condition node", "[control]") {
+  int n = 0;
+
+  // clang-format off
+  auto a =
+      bhv::fallback("a")
+        .add<bhv::condition>("a1", [] { return true; })
+        .add<bhv::action>("a2", [&n]  { n = 42; return bhv::status::success; });
+
+  auto b =
+      bhv::fallback("b")
+        .add<bhv::condition>("b1", [] { return false; })
+        .add<bhv::action>("b2", [&n]  { n = 43; return bhv::status::success; });
+
+  auto seq =
+      bhv::sequence("root")
+        .add(a)
+        .add(b);
+  // clang-format on
+
+  REQUIRE(seq() == bhv::status::success);
+  REQUIRE(n == 43);
+}
