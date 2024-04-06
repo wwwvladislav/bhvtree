c2814da1356fc60ddec1d582fb945f830c0a4a56
Vladislav Volkov <wwwvladislav@gmail.com>
2024-04-06 20:46:53 +0300

    Unit tests for fallback and parallel nodes

--- tests/basic_tests.cpp
+++ tests/basic_tests.cpp
@@ -41,3 +41,65 @@ TEST_CASE("Sequence of sequences", "[control]") {
   REQUIRE(seq() == bhv::status::success);
   REQUIRE(n == 4);
 }
+
+TEST_CASE("Fallback node", "[control]") {
+  int n = 0;
+
+  // clang-format off
+  auto seq =
+      bhv::fallback("root")
+        .add<bhv::action>("1", [&n] { n = 42; return bhv::status::failure; })
+        .add<bhv::action>("2", [&n] { n = 43; return bhv::status::success; })
+        .add<bhv::action>("3", [&n] { n = 44; return bhv::status::success; });
+  // clang-format on
+
+  REQUIRE(seq() == bhv::status::success);
+  REQUIRE(n == 43);
+}
+
+TEST_CASE("Failed fallback node", "[control]") {
+  int n = 0;
+
+  // clang-format off
+  auto seq =
+      bhv::fallback("root")
+        .add<bhv::action>("1", [&n] { n = 42; return bhv::status::failure; })
+        .add<bhv::action>("2", [&n] { n = 43; return bhv::status::failure; })
+        .add<bhv::action>("3", [&n] { n = 44; return bhv::status::failure; });
+  // clang-format on
+
+  REQUIRE(seq() == bhv::status::failure);
+  REQUIRE(n == 44);
+}
+
+TEST_CASE("Parallel node", "[control]") {
+  int n = 0;
+
+  // clang-format off
+  auto seq =
+      bhv::parallel("root", 2)
+        .add<bhv::action>("1", [&n] { ++n; return bhv::status::failure; })
+        .add<bhv::action>("2", [&n] { ++n; return bhv::status::success; })
+        .add<bhv::action>("3", [&n] { ++n; return bhv::status::success; })
+        .add<bhv::action>("4", [&n] { ++n; return bhv::status::success; });
+  // clang-format on
+
+  REQUIRE(seq() == bhv::status::success);
+  REQUIRE(n == 4);
+}
+
+TEST_CASE("Parallel fallback node", "[control]") {
+  int n = 0;
+
+  // clang-format off
+  auto seq =
+      bhv::parallel("root", 2)
+        .add<bhv::action>("1", [&n] { ++n; return bhv::status::failure; })
+        .add<bhv::action>("2", [&n] { ++n; return bhv::status::failure; })
+        .add<bhv::action>("3", [&n] { ++n; return bhv::status::success; })
+        .add<bhv::action>("4", [&n] { ++n; return bhv::status::failure; });
+  // clang-format on
+
+  REQUIRE(seq() == bhv::status::failure);
+  REQUIRE(n == 4);
+}
